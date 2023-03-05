c575e520738581a5e0da5776809fc42b6a9123e1
Jaewon Lee <joyjwlee@gmail.com>
2023-03-04 17:00:25 -0800

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,6 +12,9 @@ const config: HardhatUserConfig = {
       accounts: [process.env.PRIVATE_KEY!],
     },
   },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY,
+  },
 };
 
 export default config;
